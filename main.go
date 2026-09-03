e79022aeabe409ffbbef80405a285fecbd6d18ab
fernandezmarcosm-jpg <fernandez.marcosm@gmail.com>
2026-09-03 13:45:01 -0300

    feat: initialize persistent runtime log and recover startup panics

--- main.go
+++ main.go
@@ -3,48 +3,56 @@
 package main
 
 import (
-	"log"
-	"syscall"
-	"unsafe"
+    "syscall"
+    "unsafe"
 )
 
 var (
-	user32   = syscall.NewLazyDLL("user32.dll")
-	kernel32 = syscall.NewLazyDLL("kernel32.dll")
-	comctl32 = syscall.NewLazyDLL("comctl32.dll")
-	comdlg32 = syscall.NewLazyDLL("comdlg32.dll")
-	hInstance uintptr
+    user32   = syscall.NewLazyDLL("user32.dll")
+    kernel32 = syscall.NewLazyDLL("kernel32.dll")
+    comctl32 = syscall.NewLazyDLL("comctl32.dll")
+    comdlg32 = syscall.NewLazyDLL("comdlg32.dll")
+    hInstance uintptr
 )
 
 type winPOINT struct{ X, Y int32 }
 type winMSG struct {
-	Hwnd uintptr
-	Message uint32
-	WParam, LParam uintptr
-	Time uint32
-	Pt winPOINT
+    Hwnd uintptr
+    Message uint32
+    WParam, LParam uintptr
+    Time uint32
+    Pt winPOINT
 }
 
 // Punto de entrada único de la reconstrucción Win32.
 func main() {
-	console, _, _ := kernel32.NewProc("GetConsoleWindow").Call()
-	if console != 0 {
-		user32.NewProc("ShowWindow").Call(console, 0)
-	}
-	comctl32.NewProc("InitCommonControls").Call()
+    appLogInit()
+    defer appRecover("main")
+    appLog("EVENTO: inicio del programa")
 
-	hwnd := crearVentana()
-	if hwnd == 0 {
-		log.Fatal("No se pudo crear la ventana principal")
-	}
+    console, _, _ := kernel32.NewProc("GetConsoleWindow").Call()
+    if console != 0 {
+        user32.NewProc("ShowWindow").Call(console, 0)
+    }
+    comctl32.NewProc("InitCommonControls").Call()
+    appLog("EVENTO: common controls inicializados")
 
-	var msg winMSG
-	for {
-		ret, _, _ := user32.NewProc("GetMessageW").Call(uintptr(unsafe.Pointer(&msg)), 0, 0, 0)
-		if int32(ret) <= 0 {
-			break
-		}
-		user32.NewProc("TranslateMessage").Call(uintptr(unsafe.Pointer(&msg)))
-		user32.NewProc("DispatchMessageW").Call(uintptr(unsafe.Pointer(&msg)))
-	}
+    hwnd := crearVentana()
+    appLog("EVENTO: crearVentana => hwnd=0x%X", hwnd)
+    if hwnd == 0 {
+        appLog("ERROR: no se pudo crear la ventana principal")
+        return
+    }
+
+    var msg winMSG
+    for {
+        ret, _, _ := user32.NewProc("GetMessageW").Call(uintptr(unsafe.Pointer(&msg)), 0, 0, 0)
+        if int32(ret) <= 0 {
+            appLog("EVENTO: GetMessageW finalizó ret=%d", int32(ret))
+            break
+        }
+        user32.NewProc("TranslateMessage").Call(uintptr(unsafe.Pointer(&msg)))
+        user32.NewProc("DispatchMessageW").Call(uintptr(unsafe.Pointer(&msg)))
+    }
+    appLog("=== FIN GestionSO V57 ===")
 }
