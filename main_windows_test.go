c89fc94b0c2bd70671ac8abadc22a9b8ceac4c5a
fernandezmarcosm-jpg <fernandez.marcosm@gmail.com>
2026-09-03 15:44:30 -0300

    Validate Win64 OPENFILENAMEW layout

--- main_windows_test.go
+++ main_windows_test.go
@@ -3,13 +3,13 @@
 package main
 
 import (
-	"testing"
-	"unsafe"
+    "testing"
+    "unsafe"
 )
 
 func TestOPENFILENAMEWWin64Size(t *testing.T) {
-	const want = uintptr(144)
-	if got := unsafe.Sizeof(OPENFILENAMEW{}); got != want {
-		t.Fatalf("OPENFILENAMEW size = %d, want %d", got, want)
-	}
+    const want = uintptr(152)
+    if got := unsafe.Sizeof(OPENFILENAMEW{}); got != want {
+        t.Fatalf("OPENFILENAMEW size = %d, want %d", got, want)
+    }
 }
