b69422c66601f676d57466666442231fec1e9c6b
fernandezmarcosm-jpg <fernandez.marcosm@gmail.com>
2026-09-02 22:10:00 -0300

    Fix(main): remove duplicated constants from main.go to avoid redeclaration; keep DLLs and message loop

--- main.go
+++ main.go
@@ -15,22 +15,6 @@ var (
 	comdlg32 = syscall.NewLazyDLL("comdlg32.dll")
 )
 
-const (
-	WM_CREATE  = 0x0001
-	WM_DESTROY = 0x0002
-	WM_CLOSE   = 0x0010
-	WM_COMMAND = 0x0111
-	WM_SIZE    = 0x0005
-	BN_CLICKED = 0
-
-	ID_ABRIR_XLSX   = 1001
-	ID_RECARGAR     = 1002
-	ID_COLUMNAS     = 1003
-	ID_FILTRAR      = 1004
-	ID_EXPORTAR_CSV = 1005
-	ID_LIMPIAR      = 1006
-)
-
 type winPOINT struct{ X, Y int32 }
 type winMSG struct{ Hwnd uintptr; Message uint32; WParam, LParam uintptr; Time uint32; Pt winPOINT }
 
