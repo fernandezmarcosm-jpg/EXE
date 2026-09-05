6610468ba56d279f4b5c2d465f426bb48afe2433
fernandezmarcosm-jpg <fernandez.marcosm@gmail.com>
2026-09-04 22:26:12 -0300

    diagnostico: registrar codigos WM_NOTIFY

---
+++ notify_diagnostics_windows.go
@@ -0,0 +1,18 @@
+//go:build windows
+package main
+
+import (
+ "reflect"
+ "unsafe"
+)
+
+func columnViewLogNotify(l uintptr) {
+ if l==0 { return }
+ h:=columnViewReadNMHeader(l)
+ appLog("DIAGNOSTICO: WM_NOTIFY hWndFrom=0x%X idFrom=0x%X h.Code=%d",h.HwndFrom,h.IDFrom,h.Code)
+ if h.Code==nmDblClk { n:=columnViewReadNMItemActivate(l); appLog("DIAGNOSTICO: nmDblClk item=%d subItem=%d",n.Item,n.SubItem) }
+ if h.Code==hdnItemDblClickW || h.Code==hdnEndDrag { n:=columnViewReadNMHeaderNotify(l); appLog("DIAGNOSTICO: header notify code=%d item=%d",n.Hdr.Code,n.Item) }
+}
+
+var _ = reflect.ValueOf
+var _ = unsafe.Sizeof
