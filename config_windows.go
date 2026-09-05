6950ac7be4c6f351e7f4512738c4a4a3590e83a9
fernandezmarcosm-jpg <fernandez.marcosm@gmail.com>
2026-09-04 21:03:48 -0300

    Configuración: selector de fórmulas y editor de nombres funcional

--- config_windows.go
+++ config_windows.go
@@ -1,80 +1,8 @@
 //go:build windows
-
 package main
 
 import "strings"
 
-// configWndProc handles the controls created by datasetShowConfig.
-func configWndProc(h uintptr, m uint32, w, l uintptr) uintptr {
-    defer appRecover("configWndProc")
-    switch m {
-    case WM_COMMAND:
-        cmd := int(w & 0xffff)
-        switch cmd {
-        case configIDOK:
-            s := appSettings
-            s.Decimals = strconvSafe(appGetEdit(configEdits[configIDDecimals]), s.Decimals)
-            s.FontSize = strconvSafe(appGetEdit(configEdits[configIDFont]), s.FontSize)
-            s.SOColumn = strconvSafe(appGetEdit(configEdits[configIDSOColumn]), s.SOColumn)
-            s.JoinExcelColumn = strings.TrimSpace(appGetEdit(configEdits[configIDJoin]))
-            s.FormulaTitle = strings.TrimSpace(appGetEdit(configEdits[configIDFormulaTitle]))
-            s.Formula = strings.TrimSpace(appGetEdit(configEdits[configIDFormula]))
-            subtotalText := strings.TrimSpace(appGetEdit(configEdits[configIDSubtotal]))
-            s.SubtotalColumns = nil
-            if subtotalText != "" {
-                for _, part := range strings.Split(subtotalText, ";") {
-                    if v := strings.TrimSpace(part); v != "" {
-                        s.SubtotalColumns = append(s.SubtotalColumns, v)
-                    }
-                }
-            }
-            if len(s.SubtotalColumns) > 0 {
-                s.SubtotalColumn = s.SubtotalColumns[0]
-            } else {
-                s.SubtotalColumn = ""
-            }
-            s.MaxColumns = strconvSafe(appGetEdit(configEdits[configIDMaxColumns]), s.MaxColumns)
-            s.SubtotalEnabled = appGetCheck(configEdits[configIDSubtotalCheck])
-            datasetSettingsNormalize(&s)
-            appSettings = s
-            if err := saveDatasetSettings(appSettings); err != nil {
-                appLog("ERROR: no se pudo guardar configuración: %v", err)
-            }
-            closeConfigWindow()
-            appApplySettings()
-            return 0
-        case configIDCancel:
-            closeConfigWindow()
-            return 0
-        case configIDNames:
-            appLog("EVENTO: EDITAR NOMBRES solicitado; editor pendiente")
-            return 0
-        }
-    case WM_CLOSE, WM_DESTROY:
-        closeConfigWindow()
-        return 0
-    }
-    r, _, _ := user32.NewProc("DefWindowProcW").Call(h, uintptr(m), w, l)
-    return r
-}
-
-func appGetCheck(h uintptr) bool {
-    if h == 0 {
-        return false
-    }
-    v, _, _ := user32.NewProc("SendMessageW").Call(h, bmGetCheck, 0, 0)
-    return v == bstChecked
-}
-
-func closeConfigWindow() {
-    h := configHwnd
-    configHwnd = 0
-    configEdits = map[int]uintptr{}
-    if h != 0 {
-        user32.NewProc("DestroyWindow").Call(h)
-    }
-    if configParent != 0 {
-        appSetEnabled(configParent, true)
-        configParent = 0
-    }
-}
+func configWndProc(h uintptr,m uint32,w,l uintptr)uintptr{defer appRecover("configWndProc");if m==wmEraseBkgnd{return 1};if m==wmCtlColorStatic{b,_,_:=user32.NewProc("GetSysColorBrush").Call(5);return b};if m==WM_COMMAND{cmd:=int(w&0xffff);switch cmd{case configIDFormulaColumns:if configFormulaSelector!=0{sel,_,_:=user32.NewProc("SendMessageW").Call(configFormulaSelector,cbGetCurSel,0,0);if int(sel)>=0&&viewDataset!=nil&&int(sel)<len(viewDataset.Columns){appSetEdit(configEdits[configIDFormula],appGetEdit(configEdits[configIDFormula])+formulaTokenName(viewDataset.Columns[int(sel)]))}};return 0;case configIDOK:s:=appSettings;s.Decimals=strconvSafe(appGetEdit(configEdits[configIDDecimals]),s.Decimals);s.FontSize=strconvSafe(appGetEdit(configEdits[configIDFont]),s.FontSize);s.SOColumn=strconvSafe(appGetEdit(configEdits[configIDSOColumn]),s.SOColumn);s.JoinExcelColumn=strings.TrimSpace(appGetEdit(configEdits[configIDJoin]));s.FormulaTitle=strings.TrimSpace(appGetEdit(configEdits[configIDFormulaTitle]));s.Formula=strings.TrimSpace(appGetEdit(configEdits[configIDFormula]));subtotalText:=strings.TrimSpace(appGetEdit(configEdits[configIDSubtotal]));s.SubtotalColumns=nil;if subtotalText!=""{for _,part:=range strings.Split(subtotalText,";"){if v:=strings.TrimSpace(part);v!=""{s.SubtotalColumns=append(s.SubtotalColumns,v)}}};if len(s.SubtotalColumns)>0{s.SubtotalColumn=s.SubtotalColumns[0]}else{s.SubtotalColumn=""};s.MaxColumns=strconvSafe(appGetEdit(configEdits[configIDMaxColumns]),s.MaxColumns);s.SubtotalEnabled=appGetCheck(configEdits[configIDSubtotalCheck]);datasetSettingsNormalize(&s);appSettings=s;if err:=saveDatasetSettings(appSettings);err!=nil{appLog("ERROR: no se pudo guardar configuración: %v",err)};closeConfigWindow();appApplySettings();return 0;case configIDCancel:closeConfigWindow();return 0;case configIDNames:columnViewNames();return 0}};if m==WM_CLOSE||m==WM_DESTROY{closeConfigWindow();return 0};r,_,_:=user32.NewProc("DefWindowProcW").Call(h,uintptr(m),w,l);return r}
+func appGetCheck(h uintptr)bool{if h==0{return false};v,_,_:=user32.NewProc("SendMessageW").Call(h,bmGetCheck,0,0);return v==bstChecked}
+func closeConfigWindow(){h:=configHwnd;configHwnd=0;configEdits=map[int]uintptr{};configFormulaSelector=0;if h!=0{user32.NewProc("DestroyWindow").Call(h)};if configParent!=0{appSetEnabled(configParent,true);configParent=0}}
