22ede42f43e2529a4ab074136110d9ab6c6c734c
fernandezmarcosm-jpg <fernandez.marcosm@gmail.com>
2026-09-09 19:22:11 -0300

    Disparar corrección final del handler LVN

--- column_view_notify_windows.go
+++ column_view_notify_windows.go
@@ -4,7 +4,7 @@ package main
 import ("reflect"; "syscall"; "unsafe")
 
 // LVN_GETDISPINFOW is handled in column_view_windows.go.
-// Trigger automatic correction commit with write permission.
+// Trigger final handler correction.
 func columnViewCopyNotify(dst interface{}, src uintptr, size uintptr) {
 	k := syscall.NewLazyDLL("kernel32.dll")
 	k.NewProc("RtlMoveMemory").Call(reflect.ValueOf(dst).Pointer(), src, size)
