6ef8f2c73d346d47bb727248ab6520b7d06a1a11
fernandezmarcosm-jpg <fernandez.marcosm@gmail.com>
2026-09-10 12:43:20 -0300

    Keep imported values static; remove automatic refresh

--- filter_input_fix_windows.go
+++ filter_input_fix_windows.go
@@ -11,8 +11,6 @@ const (
     filterFixEnChange         = 0x0300
     filterFixEnSetFocus       = 0x0100
     filterFixEnKillFocus      = 0x0200
-    filterFixWmKeyDown        = 0x0100
-    filterFixVkReturn         = 0x000D
 )
 
 var filterFixOldWndProc uintptr
@@ -40,18 +38,10 @@ func filterFixInstall(hwnd uintptr) {
     if old == 0 { return }
     filterFixOldWndProc = old
     filterFixInstalled = true
-    appLog("DIAGNOSTICO: filtro: refresco automático deshabilitado; aplicar con ENTER")
+    appLog("DIAGNOSTICO: filtro: refresco automático deshabilitado; valores quedan estancos")
 }
 
 func filterFixWndProc(hwnd uintptr, msg uint32, w, l uintptr) uintptr {
-    if msg == filterFixWmKeyDown && w == filterFixVkReturn {
-        for _, h := range viewFilters {
-            if h != 0 && filterFixIsEditFocused(h) {
-                filterFixApplyFocusedFilter(h)
-                return 0
-            }
-        }
-    }
     if msg == WM_COMMAND {
         id := int(w & 0xffff)
         notify := int((w >> 16) & 0xffff)
@@ -62,22 +52,6 @@ func filterFixWndProc(hwnd uintptr, msg uint32, w, l uintptr) uintptr {
         }
     }
     return filterFixCallOld(hwnd, msg, w, l)
-}
-
-func filterFixApplyFocusedFilter(editHwnd uintptr) {
-    for id, h := range viewFilters {
-        if h == editHwnd {
-            columnViewHandleFilterChange(uintptr(filterBaseID))
-            appLog("DIAGNOSTICO: filtro aplicado explícitamente con ENTER; columna=%s", id)
-            return
-        }
-    }
-}
-
-func filterFixIsEditFocused(target uintptr) bool {
-    p := syscall.NewLazyDLL("user32.dll")
-    focused, _, _ := p.NewProc("GetFocus").Call()
-    return focused == target
 }
 
 func filterFixCallOld(hwnd uintptr, msg uint32, w, l uintptr) uintptr {
