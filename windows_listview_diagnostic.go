4faeb4aa349d4bfce5c393de3d61b0d254b3b7e7
fernandezmarcosm-jpg <fernandez.marcosm@gmail.com>
2026-09-10 10:51:35 -0300

    Fix diagnostic build errors on Go 1.24

--- windows_listview_diagnostic.go
+++ windows_listview_diagnostic.go
@@ -12,8 +12,6 @@ const (
     diagGWChild         = 5
     diagGWHWndNext      = 2
     diagGWHWndPrev      = 3
-    diagGWLStyle        = -16
-    diagGWLExStyle      = -20
     diagLVMGetItemTextW = 0x1073
     diagLVIFText        = 0x0001
 )
@@ -28,7 +26,7 @@ type diagLVItem struct {
     Image, Param int32
 }
 
-func diagCall(proc *syscall.LazyProc, args ...uintptr) (uintptr, uintptr, syscall.Errno) { return proc.Call(args...) }
+func diagCall(proc *syscall.LazyProc, args ...uintptr) (uintptr, uintptr, error) { return proc.Call(args...) }
 
 func diagClass(hwnd uintptr) string {
     if hwnd == 0 { return "<0>" }
@@ -53,8 +51,9 @@ func diagRectOf(hwnd uintptr) (diagRect, bool) {
 }
 
 func diagStyle(hwnd uintptr) (uintptr, uintptr) {
-    styleIndex := uintptr(int64(diagGWLStyle))
-    exStyleIndex := uintptr(int64(diagGWLExStyle))
+    // GWL_STYLE=-16 and GWL_EXSTYLE=-20 represented without constant conversion overflow.
+    styleIndex := ^uintptr(15)
+    exStyleIndex := ^uintptr(19)
     s, _, _ := diagCall(user32.NewProc("GetWindowLongPtrW"), hwnd, styleIndex)
     e, _, _ := diagCall(user32.NewProc("GetWindowLongPtrW"), hwnd, exStyleIndex)
     return s, e
