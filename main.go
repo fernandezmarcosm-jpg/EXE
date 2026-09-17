e74f8437ea3c8fbb9c100c771467c909f359bc32
fernandezmarcosm-jpg <fernandez.marcosm@gmail.com>
2026-09-17 16:52:25 -0300

    Use Wails v2 options assetserver package

--- main.go
+++ main.go
@@ -5,8 +5,8 @@ import (
 	"log"
 
 	"github.com/wailsapp/wails/v2"
-	"github.com/wailsapp/wails/v2/pkg/assetserver"
 	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
 //go:embed all:frontend/dist
